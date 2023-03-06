86210c27131a335ee00fd2dd6936a14663f15958
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-03-06 21:28:56 +0200

    delete console

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,7 +10,6 @@ export function App() {
   const [good, setGood] = useState(0);
   const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
-  console.log('h');
   
   const feedbackOptions = { good,bad,neutral };
   
